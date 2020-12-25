51814cfef22440b9ff6149a8d914278b32db2da4
Shaqqour <mshaqqourm@gmail.com>
2020-12-24 23:48:43 -0800

    Writes getAuthState to get the token and the user within try and catch block

--- app/providers/auth_provider.js
+++ app/providers/auth_provider.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import { AsyncStorage } from 'react-native';
 
 
 
@@ -7,4 +8,33 @@ const AuthContext = React.createContext();
 
 function AuthProvider(props) {
     const [state, dispatch] = useReducer(reducer, initialState || {});
+
+    // Get Auth state
+    const getAuthState = async () => {
+        try {
+            // Get token && user
+            let token = await AsyncStorage.getItem(TOKEN_KEY);
+            let user = await AsyncStorage.getItem(USER_KEY);
+            user = JSON.parse(user);
+
+            if (token !== null && user !== null) await handleLogin({ token, user });
+            else await handleLogout();
+
+            return {token, user};
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    //Handle Login
+    const handleLogin = async(data) => {
+        //do something
+    }
+
+    //Handle Logout
+    const handleLogin = async () => {
+        //do something
+    }
+
 }
