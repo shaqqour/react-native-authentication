6bd926af954a4f17a8004e6e78c77f1c3c34846d
Shaqqour <mshaqqourm@gmail.com>
2021-01-01 09:25:21 -0800

    Exports  config keys the token and the user key

--- app/providers/auth_provider.js
+++ app/providers/auth_provider.js
@@ -5,7 +5,10 @@ import { AsyncStorage } from 'react-native';
 //Import reducer, initial state and action types
 import reducer, {initialState, LOGGED_IN, LOGGED_OUT} from "./reducer";
 
-
+//Config keys
+export const TOKEN_KEY = 'token';
+export const USER_KEY = "user";
+export const keys = [TOKEN_KEY, USER_KEY];
 
 //Context
 const AuthContext = React.createContext();
