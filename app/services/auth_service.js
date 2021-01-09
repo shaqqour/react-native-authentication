5fc0ea82e54f2ba1ce739fde5a02cde199547b28
Shaqqour <mshaqqourm@gmail.com>
2021-01-09 12:23:15 -0800

    Writes the async function for error handling

--- app/services/auth_service.js
+++ app/services/auth_service.js
@@ -48,3 +48,13 @@ export async function updateProfile(userId, data) {
         throw handler(e);
     }
 }
+
+export function handler(err) {
+    let error = err;
+
+    if (err.response && err.response.data.hasOwnProperty("message"))
+        error = err.response.data;
+    else if (!err.hasOwnProperty("message")) error = err.toJSON();
+
+    return new Error(error.message);
+}
