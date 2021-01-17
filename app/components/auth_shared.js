317be9c4c11d25b2f41fafc646523697c0ba30c4
Shaqqour <mshaqqourm@gmail.com>
2021-01-17 03:00:19 -0800

    Creates the shared.js and declares the header

---
+++ app/components/auth_shared.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+//HEADER COMPONENT
+export const Header = (props) => {
+    let { title, style } = props;
+
+    return (
+        <View style={[styles.header, style]}>
+            <Text style={styles.headerText}>
+                {title}
+            </Text>
+        </View>
+    )
+};
