0ebe14cee1811cb5c2d66080da4c6b4ccb9d1f4f
Shaqqour <mshaqqourm@gmail.com>
2021-02-06 04:18:13 -0800

    Declares the login fields

--- app/scenes/auth/auth_login.js
+++ app/scenes/auth/auth_login.js
@@ -7,3 +7,18 @@ import { useAuth } from "../../providers/auth";
 import Form from 'react-native-basic-form';
 import CTA from "../../components/CTA";
 import { Header, ErrorText } from "../../components/Shared";
+
+export default function Login(props) {
+    const { navigation } = props;
+    const { navigate } = navigation;
+
+    //1 - DECLARE VARIABLES
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const { handleLogin } = useAuth();
+
+    const fields = [
+        { name: 'email', label: 'Email Address', required: true },
+        { name: 'password', label: 'Password', required: true, secure: true }
+    ];
+};
