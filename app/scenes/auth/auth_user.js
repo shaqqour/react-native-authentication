7512293a5643bdc524b71a15a8247d829547424f
Shaqqour <mshaqqourm@gmail.com>
2021-02-20 19:33:24 -0800

    Creates auth_user and imports required packages

---
+++ app/scenes/auth/auth_user.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Alert, View } from 'react-native';
+
+import * as api from "../../services/auth";
+
+import Form from 'react-native-basic-form';
+import { Header, ErrorText } from "../../components/Shared";
