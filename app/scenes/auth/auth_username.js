a8dce8fe21d0c19aecc55c65bb6988e954c03f26
Shaqqour <mshaqqourm@gmail.com>
2021-03-02 08:49:30 -0800

    Imports useAuth from providers

--- app/scenes/auth/auth_username.js
+++ app/scenes/auth/auth_username.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Alert, View } from 'react-native';
+import { View } from 'react-native';
 
 import * as api from "../../services/auth";
+import { useAuth } from "../../providers/auth";
 
 import Form from 'react-native-basic-form';
 import { Header, ErrorText } from "../../components/Shared";
