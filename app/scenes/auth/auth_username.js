47a6985aff31e12e60b1bab1a58a11a314edc0ec
Shaqqour <mshaqqourm@gmail.com>
2021-03-04 21:07:42 -0800

    Declare some variables and assign props to navigation object

--- app/scenes/auth/auth_username.js
+++ app/scenes/auth/auth_username.js
@@ -6,3 +6,13 @@ import { useAuth } from "../../providers/auth";
 
 import Form from 'react-native-basic-form';
 import { Header, ErrorText } from "../../components/Shared";
+
+export default function Username(props) {
+    const { navigation } = props;
+
+    // DECLARE VARIABLES
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const { state, updateUser } = useAuth();
+
+};
